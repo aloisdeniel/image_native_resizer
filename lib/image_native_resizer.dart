53b0b3ea904ccfda9c98314da91c427594a95695
Aloïs Deniel <alois.deniel@gmail.com>
2020-04-29 15:52:50 +0200

    * Add documentation. * Add null checks.

--- lib/image_native_resizer.dart
+++ lib/image_native_resizer.dart
@@ -7,12 +7,21 @@ class ImageNativeResizer {
   static const MethodChannel _channel =
       const MethodChannel('image_native_resizer');
 
+  /// Resizes the image at the given [imagePath] to the given
+  /// [maxWidth], [maxHeight] and/or [quality].
+  ///
+  /// If the image size is smaller than the provided [maxWidth] and/or
+  /// [maxHeight], the original image can be returned.
+  ///
+  /// The [quality] must be a value between `0` and `100`.
   static Future<String> resize({
     @required String imagePath,
     double maxWidth,
     double maxHeight,
     int quality,
   }) async {
+    assert(imagePath != null);
+    assert(quality == null || (quality >= 0 && quality <= 100));
     final path = await _channel.invokeMethod('resize', {
       'imagePath': imagePath,
       'maxWidth': maxWidth,
